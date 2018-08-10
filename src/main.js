32f87ef35565256a0d21bc3f394347c7e9d7838d
nmenescardi <nicolasmenescardi@hotmail.com>
2018-08-10 18:38:55 -0300

    Add filter

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import router from './router'
 window.axios = require('axios')
 Vue.config.productionTip = false
 
+Vue.filter('readMore', function (text, length, suffix) {
+  return text.substring(0, length) + suffix
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
